ac74688e36eda9c2d23f9b5d6544aa558b2b3682
Inho <limcara@nate.com>
2019-02-20 16:49:28 +0900

    모델 menua 추가

--- menu/models.py
+++ menu/models.py
@@ -6,3 +6,7 @@ class Menu(models.Model):
     name = models.CharField(max_length=100)
     amount = models.PositiveIntegerField()
     image = models.ImageField()
+
+class Menua(models.Model):
+    name = models.CharField(max_length=200)
+    
